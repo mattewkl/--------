5078726234230a8e25b5e177851dc0ec1579ba3b
Матвей Климовский <cikle290@gmail.com>
2026-08-28 15:24:21 +1000

    init commit

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="page">
+    <h1>Nuxt 4</h1>
+    <p>Проект запущен на localhost.</p>
+  </div>
+</template>
+
+<style scoped>
+.page {
+  font-family: system-ui, sans-serif;
+  padding: 2rem;
+}
+</style>
